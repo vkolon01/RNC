b12fce7e24fb7a78d43e3445f959a38f2d275cda
vkolon01 <rockasalata2016>
2017-09-01 18:41:13 +0100

    todo:
    Throw error on input < 0
    Return the correct format on GET request
    Implement roman to numeral
    Simplify the roman symbols representation (too many doublicates  such as "MMMM")

--- app.js
+++ app.js
@@ -6,20 +6,38 @@ var mongoose = require('mongoose');
 var validator = require('validator');
 var Promise = require('promise');
 
-var convertedSchema = new mongoose.Schema({arabic:String, roman:String});
-var Converted = mongoose.model('Converted',convertedSchema);
+// Mongodb schema definition.
+//var convertedSchema = new mongoose.Schema({arabic:String, roman:String});
+var Converted = mongoose.model('Converted',{arabic:String, roman:String});
+
+// Connection to a local mongodb address.
+mongoose.connect("mongodb://localhost:27017/numbers",function(err,db){
+    if(!err){
+        console.log("MongoDB connection successful");
+    }
+});
 
 app.use(bodyParser.json());
-
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
-    next();
-});
 
 app.get('/roman/:number',convert);
 app.get('/all/:numeralType',getAll);
 
+
+// Collection of characters that represent the roman numeric system.
+// The integer value of each roman numeral is the index of the array.
+const numericSystem = {
+    romanOnes : ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    romanTens : ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    romanHundreds : ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    romanThousands : ["", "M", "MM", "MMM", "MMMM"]
+};
+
+/**
+ *  Fetches the entire collection of previously converted numbers and returns in a type requested by the url parameter.
+ *
+ * @param req.params.numeralType given through url.
+ * @returns {*}
+ */
 function getAll(req,res){
     return new Promise(function(fulfill,reject){
         var collection = [];
@@ -30,17 +48,25 @@ function getAll(req,res){
 }
 
 /**
- *  Sends a converted number back to the user.
- *  Returns error code 400 if bad input.
+ * Converts the given input to either integer or roman numeral accordingly.
+ * Returns JSON string of both, input value and converted value.
  *
- * @param req
- * @param res
+ * @param req.params.number given through url.
+ * @returns converted number back to the user.
+ * @returns error code 400 if bad input.
+ * @returns error code 500 if mongoDB error.
  */
 function convert(req,res){
     var badInputError = 'Please provide input of type Integer (4999 or less) or Roman numeral for conversion';
     var param = req.params.number;
     if(isNaN(param)){
-        console.log('not a number')
+        if(typeof param === 'string'){
+            romanToInt(param).then(function(result){
+                res.json(result);
+            });
+        }else{
+            res.status(400).end(http.STATUS_CODES[400] + '\n' + badInputError);
+        }
     }else{
         if(validator.isInt(param)){
             if(param < 5000){
@@ -53,7 +79,6 @@ function convert(req,res){
                             res.json([param,result.roman]);
                         }
                     });
-
                 },function(err){
                     res.status(400).end(http.STATUS_CODES[400] + '\n' + err);
                 })
@@ -66,37 +91,54 @@ function convert(req,res){
     }
 }
 
+function romanToInt(romanNumber){
+    return new Promise(function(fulfill,reject){
+        var splitNumber = romanNumber.toUpperCase().split('');
+        var length = splitNumber.length;
+        var result = [];
 
+        for(var i = length - 1; i >= 0; i--){
+            var curNum = 0;
+            var composition = '';
+            var integer = numericSystem.romanOnes.indexOf(splitNumber[i] + composition);
+            if(integer != -1){
+                composition = splitNumber[i] + composition;
+            }else{
+                result.unshift(curNum);
+                composition = '';
+            }
+        }
+        fulfill(result);
+    });
+}
+
+/**
+ * Separates the decimal into up to four parts (thousands,hundreds,tens and ones). Then converting each number according to the given alphabet.
+ *
+ * @param integerNumber
+ * @returns {*}
+ */
 function intToRoman(integerNumber){
     return new Promise(function(fulfill,reject){
         var splitNumber = integerNumber.split('');
-        var romanOnes = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-        var romanTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-        var romanHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-        var romanThousands = ["", "M", "MM", "MMM", "MMMM"];
-        var numberSize = splitNumber.length;
+        var length = splitNumber.length;
         var romanNumber = [];
-        for(var i = numberSize; i > 0; --i){
+
+        for(var i = length; i > 0; --i){
             var curNum = parseInt(splitNumber[i-1]);
-            if(i == numberSize){
-                romanNumber.unshift(romanOnes[curNum])
-            }else if(i == numberSize - 1){
-                romanNumber.unshift(romanTens[curNum])
-            }else if(i == numberSize -2){
-                romanNumber.unshift(romanHundreds[curNum])
+            if(i == length){
+                romanNumber.unshift(numericSystem.romanOnes[curNum])
+            }else if(i == length - 1){
+                romanNumber.unshift(numericSystem.romanTens[curNum])
+            }else if(i == length -2){
+                romanNumber.unshift(numericSystem.romanHundreds[curNum])
             }else{
-                romanNumber.unshift(romanThousands[curNum])
+                romanNumber.unshift(numericSystem.romanThousands[curNum])
             }
         }
         fulfill({arabic:integerNumber,roman:romanNumber.join("")}); //Returns a converted roman number with type string
     });
 }
-
-mongoose.connect("mongodb://localhost:27017/numbers",function(err,db){
-    if(!err){
-        console.log("MongoDB connection successful");
-    }
-});
 
 var server = app.listen(5000,function(){
     console.log('Listening on port ' + server.address().port);
